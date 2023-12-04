5c9768ae72665e011e0a2e3c64c761764c405302
celiaescribe <celia.escribe@polytechnique.edu>
2023-12-04 14:15:04 +0100

    update ResIRF

--- eoles/model_resirf_coupling.py
+++ eoles/model_resirf_coupling.py
@@ -684,19 +684,19 @@ class ModelEOLES():
         """Attention au choix de la infeasible_value: c'est la valeur que l'on donne lorsque le problème n'est pas solvable."""
         self.opt = SolverFactory(solver_name)
         self.logger.info("Solving EOLES model using %s", self.opt.name)
-        # self.solver_results = self.opt.solve(self.model,
-        #                                      options={'Presolve': 2, 'LogFile': self.path + "/logfile_" + self.name})
-
         self.solver_results = self.opt.solve(self.model,
-                                             options={'threads': 4,
-                                                      'method': 2, # barrier
-                                                      'crossover': 0,
-                                                      'BarConvTol': 1.e-6,
-                                                       'Seed': 123,
-                                                       'AggFill': 0,
-                                                       'PreDual': 0,
-                                                       'GURO_PAR_BARDENSETHRESH': 200,
-                                                      'LogFile': self.path + "/logfile_" + self.name})
+                                             options={'Presolve': 2, 'LogFile': self.path + "/logfile_" + self.name})
+
+        # self.solver_results = self.opt.solve(self.model,
+        #                                      options={'threads': 4,
+        #                                               'method': 2, # barrier
+        #                                               'crossover': 0,
+        #                                               'BarConvTol': 1.e-6,
+        #                                                'Seed': 123,
+        #                                                'AggFill': 0,
+        #                                                'PreDual': 0,
+        #                                                'GURO_PAR_BARDENSETHRESH': 200,
+        #                                               'LogFile': self.path + "/logfile_" + self.name})
 
         status = self.solver_results["Solver"][0]["Status"]
         termination_condition = self.solver_results["Solver"][0]["Termination condition"]
