132553b3e099589e4a632e0ae766012212e92cb1
celiaescribe <celia.escribe@polytechnique.edu>
2024-02-22 11:24:30 +0100

    add script for cluster

---
+++ script_ban.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+## BEGIN SBATCH directives
+#SBATCH --job-name=batch1
+#SBATCH --output="eoles/outputs/batch1.txt"
+#SBATCH --error="eoles/outputs/error_batch1.txt"
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=40
+#SBATCH --time=100:00:00
+#SBATCH --partition=cpu_shared
+#SBATCH --account=gennn
+#SBATCH --nodelist=node019
+#SBATCH --mem=MaxMemPerNode
+## END SBATCH directives
+
+## load modules
+module purge
+module load anaconda3/2020.11 #cuda/10.2
+module load gurobi
+export GRB_LICENSE_FILE="/mnt/beegfs/softs/opt/core/gurobi/9.5.2/linux64/gurobi.19.lic"
+conda activate envCoupling
+
+python scenarios.py
+python main_coupling_resirf.py --cpu 8 --configdir "eoles/inputs/xps/20240222" --configref "settings_framework.json"
